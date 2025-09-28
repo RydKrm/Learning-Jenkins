92726363f7ee99b2fba6353bc341d50dc6a7249b
Al Riyad Karim <124508907+RydKrm@users.noreply.github.com>
2025-09-28 15:29:15 +0600

    project intial port is changed

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== "test") {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
